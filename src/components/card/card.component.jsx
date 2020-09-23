9320ae10129f56cfcce2c3f1b38ed5e107f74618
MadaZZ <sparshranaazz@gmail.com>
2020-09-23 17:48:55 +0530

    Added styling for card component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './card.styles.css';
 
 export const Card = (props) => {
-    return <div className='card' >
+    return <div className='card-container' >
         <h1>{props.monster.name}</h1>
     </div>
 }
