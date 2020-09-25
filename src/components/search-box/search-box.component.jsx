6f5e2a1e1dc219efad505e338698c259c074da20
MadaZZ <sparshranaazz@gmail.com>
2020-09-25 11:40:00 +0530

    Fixed search component

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import './search-box.styles.css';
 
 export const SearchBox = ({ placeholder, handleChange }) => {
-    return <div className='search' >
-        <input type='search'
+    return <input className='search' type='search'
             placeholder={placeholder}
             onChange={handleChange}
         />
-    </div>
+    
 }
